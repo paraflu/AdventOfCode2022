fbf933d28e9f62576d13cdada3ac68c52960e526
Andrea Forlin <a.forlin@dataservices.it>
2022-12-06 18:09:09 +0100

    new: dev: day 5

--- fsharp/Day5.Tests/UnitTest1.fs
+++ fsharp/Day5.Tests/UnitTest1.fs
@@ -13,11 +13,72 @@ let Test1 () =
 [Z] [M] [P]
  1   2   3 """
 
-    let columns = Day5.Part1.parse initialState
+    let crateHolder = Day5.Part1.parse initialState
 
-    Assert.AreEqual(seq { 'N', 'Z' }, columns |> Seq.head)
-    let action = "move 1 from 2 to 1"
+    Assert.AreEqual(3, crateHolder |> List.length)
 
-    // let state = Day5.Part1.processAction action
+    //crateHolder |> List.head |> List.map (fun s -> s.Badge) |> printf "%A\n"
 
-    Assert.AreEqual("DCP", "")
+    Assert.AreEqual(3, crateHolder |> List.head |> List.length)
+    Assert.AreEqual(seq {" "; "N" ; "Z"}, crateHolder |> List.head |> List.map (fun s -> s.Badge))
+    Assert.AreEqual(seq {"D"; "C" ; "M"}, crateHolder |> List.tail |> List.head |> List.map (fun s -> s.Badge ))
+    Assert.AreEqual(seq {" "; " "; "P"}, crateHolder 
+                                                |> List.tail 
+                                                |> List.tail 
+                                                |> List.head 
+                                                |> List.map (fun s -> s.Badge ))
+
+[<Test>]
+let Test2 () =
+    let initialState = """
+    [D]     [B]
+[N] [C]
+[Z] [M] [P] [N]
+ 1   2   3   4"""
+
+    let crateHolder = Day5.Part1.parse initialState
+
+    Assert.AreEqual(4, crateHolder |> List.length)
+
+    //crateHolder |> List.head |> List.map (fun s -> s.Badge) |> printf "%A\n"
+
+    Assert.AreEqual(3, crateHolder |> List.head |> List.length)
+    Assert.AreEqual(seq {" "; "N" ; "Z"}, crateHolder |> List.head |> List.map (fun s -> s.Badge))
+    Assert.AreEqual(seq {"D"; "C"; "M"}, crateHolder |> List.tail |> List.head |> List.map (fun s -> s.Badge ))
+    Assert.AreEqual(seq {" "; " ";"P"}, crateHolder 
+                                                |> List.tail 
+                                                |> List.tail 
+                                                |> List.head 
+                                                |> List.map (fun s -> s.Badge ))
+    Assert.AreEqual(seq {"B"; " ";"N"}, crateHolder 
+                                        |> List.rev 
+                                        |> List.head
+                                        |> List.map (fun s -> s.Badge))
+
+[<Test>]
+let Test3 () =
+    let initialState = """
+    [D]     [B]
+[N] [C]
+[Z] [M] [P] [N]
+ 1   2   3   4"""
+
+    let cmd = "move 1 from 2 to 1" |> Part1.parseCommand |> Seq.head
+
+    Assert.AreEqual(0, cmd.move)
+    Assert.AreEqual(1, cmd.src)
+    Assert.AreEqual(0, cmd.dest)
+
+    let crates = Part1.parse initialState
+
+
+    let newCrate = Part1.execCommand crates cmd
+
+    printf "Before\n"
+    crates |> Part1.printCrateHolder 
+    printf "After\n"
+    newCrate |> Part1.printCrateHolder
+
+    let x = ["N";"Z"]
+    Assert.AreEqual(x, 
+        newCrate |> List.head |> List.map (fun s -> s.Badge))
--- fsharp/Day5/Library.fs
+++ fsharp/Day5/Library.fs
@@ -2,25 +2,108 @@
 
 module Part1 =
 
+  
+
+
     type Crate(tag: string) =
-        let _tag = tag
+        let _tag = match tag.[0] with
+                    | '[' -> tag.Trim().Substring(1,1)
+                    | _ -> tag
 
         member this.ToString = Printf.sprintf "[%s] " _tag
+        member this.Badge with get() = _tag
 
-    let parse (content: string) =
-        content.Split("\r\n")
+    type Stack = list<Crate>
+    type CrateHolder = list<Stack>
+
+    let toString (c:Crate) = c.ToString
+        
+  
+
+
+    let private removeLast (s:CrateHolder):CrateHolder = s |> List.rev |> List.tail |> List.rev
+
+    let private normalizeLength crateholder:CrateHolder = 
+        let maxSize = crateholder 
+                        |> List.map List.length 
+                        |> List.max
+
+        crateholder |> List.map (fun (l:Stack) -> match l |> List.length with
+                                            | x when x = maxSize -> l
+                                            | x -> [ 
+                                                     l; 
+                                                     [ for i in 1 .. maxSize - x -> Crate("[ ]") ]
+                                                   ] |> List.concat 
+                                 )
+    let printCrateHolder (s:CrateHolder) = 
+        let print (rowId:int) (c:list<Crate>) =
+            printf "Row %d\t" rowId
+            c |> List.iteri (fun i x -> match x.Badge with
+                                        | " " when rowId = 0 -> printf " %s  \t" " "
+                                        | _ -> printf "%s\t" x.ToString
+                                        )
+            printf "\n"
+
+        let colNo = s.Length
+        s 
+        |> normalizeLength
+        |> List.transpose
+        |> (fun l -> 
+                l |> List.iteri print
+                printf "\t\t"
+                [1..colNo] |> List.iter (fun idx -> printf " %d  \t" idx)
+                printf "\n"
+        )
+
+    let parse (content: string):CrateHolder =
+        content.Split("\r\n") 
+        |> Seq.filter (fun s -> s.Length > 0)
         |> Seq.map
             (fun r ->
                 r
                 |> Seq.chunkBySize 4
-                |> Seq.map
-                    (fun s ->
-                        s
-                        |> string
-                        |> (fun s -> s.Trim())
-                        |> (fun x -> Crate(x))))
-        |> Seq.transpose
+                |> Seq.map (fun s -> s
+                                    |> System.String 
+                                    |> (fun s -> s.Trim().PadLeft(1))
+                                    |> Crate) 
+                |> Seq.toList
+            )
+        |> Seq.toList
+        |> removeLast 
+        |> normalizeLength
+        |> List.transpose
 
     let solve (content: string) = 0
 
     let processAction (action: string) = 0
+
+
+    // move 1 from 2 to 1
+    // move 3 from 1 to 3
+    // move 2 from 2 to 1
+    // move 1 from 1 to 2
+    type Command = { move: int; src: int; dest: int; }
+      
+
+    let parseCommand (cmd:string): seq<Command> =
+        let dec x = x - 1
+        cmd.Split("\r\n")
+        |> Seq.map (fun c -> 
+                            let tok = c.Split(" ")
+                            {
+                                move = tok.[1] |> int |> dec;
+                                src = tok.[3]|> int |> dec ;
+                                dest = tok.[5]|> int |> dec
+                            })
+
+    let execCommand (crateHolder: CrateHolder) (c:Command): CrateHolder =
+        let toMove: Stack = crateHolder.[c.src].[0..c.move] |> List.rev
+        
+        crateHolder |> List.mapi (fun i s -> match i with
+                                             | x when x = c.src -> s.[c.move..]
+                                             | x when x = c.dest ->  seq { 
+                                                toMove; 
+                                                s |> List.filter (fun s -> match s.Badge with
+                                                                           | " " -> false
+                                                                           | _ -> true)} |> List.concat
+                                             | _ -> s)
